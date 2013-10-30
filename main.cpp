d5aa6856161099b0c6ef48c9ee708ac99eae1d76
VShader <t.weitz66@web.de>
2013-10-30 19:57:39 +0100

    Long life King Deadend

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "graph.hpp"
+#include <Windows.h>
 using namespace std;
 
 int main()
@@ -9,5 +10,6 @@ int main()
     test.fileInput("Adjacency Matrix.txt");
 //    test.keyboardInput();
     test.depthSearch(1, 4);
+	system("PAUSE");
     return 0;
 }
